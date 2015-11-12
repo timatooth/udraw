298a406f89be1b2a12cea1508a46394d821bb4c1
Tim Sullivan <tsullivan@timatooth.com>
2015-11-12 21:45:01 +1300

    Added ESLint JS Checking to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
+var eslint = require('gulp-eslint');
 
 gulp.task('uglifyjs', function () {
     return gulp.src('public/js/canvaspaint.js')
@@ -23,4 +24,17 @@ gulp.task('minify-css', function () {
             .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', ['uglifyjs', 'copystatic', 'minify-css']);
+gulp.task('lint', function () {
+    return gulp.src(['public/**/*.js', 'server.js', 'gulpfile.js'])
+            // eslint() attaches the lint output to the "eslint" property 
+            // of the file object so it can be used by other modules. 
+            .pipe(eslint())
+            // eslint.format() outputs the lint results to the console. 
+            // Alternatively use eslint.formatEach() (see Docs). 
+            .pipe(eslint.format())
+            // To have the process exit with an error code (1) on 
+            // lint error, return the stream and pipe to failAfterError last. 
+            .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint', 'uglifyjs', 'copystatic', 'minify-css']);
