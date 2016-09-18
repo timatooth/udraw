5cfb9be6c01fdb7839997d8a9b0639813b76b17e
Tim Sullivan <tsullivan@timatooth.com>
2016-09-18 15:28:42 +1200

    update deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,7 +19,7 @@
 
 # role :app, %w{deploy@example.com}, my_property: :my_value
 #role :app, %w{tim@origin.udraw.me}
-role :app, %w{tim@staging.udraw.me}
+role :app, %w{codeship-udraw@udraw.me}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
