09450cfef83cffd019a84e2d6806924c831045a0
Tim Sullivan <tim.s@hotdoc.com.au>
2020-11-03 17:26:09 +1300

    Got webrtc video working one way.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -32,6 +32,7 @@ export class UdrawApp extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.onConnectClick = this.onConnectClick.bind(this);
         this.onPointerMove = this.onPointerMove.bind(this);
+        this.onCallClick = this.onCallClick.bind(this);
     }
 
     onPointerMove(event){
@@ -49,6 +50,34 @@ export class UdrawApp extends React.Component {
     componentDidMount(){
 
         this.props.pixiApp.stage.on('pointermove', this.onPointerMove)
+
+
+        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        let self = this;
+        this.peer.on('call', function(call) {
+            console.log("Got a call coming in!!!")
+            getUserMedia({video: true, audio: true}, function(stream) {
+                call.answer(stream); // Answer the call with an A/V stream.
+                call.on('stream', function(remoteStream) {
+                    let videoElement = document.createElement('video');
+                    videoElement.srcObject = remoteStream;
+                    videoElement.autoplay = true; //unsure if needed
+                    let videoSprite = PIXI.Sprite.from(videoElement);
+
+                    videoSprite.anchor.set(0.5);
+                    videoSprite.width = 50;
+                    videoSprite.height = 50;
+                    videoSprite.x = 0;
+                    videoSprite.y = 50;
+
+                    let container = self.props.pixiApp.stage.getChildByName(call.peer)
+                    container.addChild(videoSprite);
+
+                });
+            }, function(err) {
+                console.log('Failed to get local stream' ,err);
+            });
+        });
 
         this.peer.on('connection', (conn) => {
             conn.on('data', (data) => {
@@ -97,6 +126,21 @@ export class UdrawApp extends React.Component {
         });
     }
 
+    onCallClick(){
+        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        let peerToConnectTo = this.state.connectToPeer;
+        let peer = this.peer;
+        getUserMedia({video: true, audio: true}, function(stream) {
+            console.log("calling", peerToConnectTo, "...")
+            var call = peer.call(peerToConnectTo, stream);
+            call.on('stream', function(remoteStream) {
+                // Show stream in some video/canvas element.
+            });
+        }, function(err) {
+            console.log('Failed to get local stream' ,err);
+        });
+    }
+
     render() {
         return (
             <div>
@@ -104,6 +148,7 @@ export class UdrawApp extends React.Component {
             <label htmlFor="connectToPeer">Connect To:</label>
             <input name="connectToPeer" onChange={this.handleChange} type="text" defaultValue={this.state.connectToPeer} />
             <button onClick={this.onConnectClick}>Connect</button>
+            <button onClick={this.onCallClick}>Call</button>
             <p>{this.state.lastMessage}</p>
             <p>x,y: {this.state.tool.x},{this.state.tool.y}</p>
             </div>
