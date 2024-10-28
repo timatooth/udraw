e88bd7048c164cc7eadeeb2b78333e63c1b6fcf0
Tim Sullivan <tsullivan@timatooth.com>
2024-10-28 23:59:20 +1300

    Sqlite for dev/test cause pg no good on GH actions

--- config/test.exs
+++ config/test.exs
@@ -6,12 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :udraw, Udraw.Repo,
-  username: "postgres",
-  password: "foobar1234t3t4turboxyz",
-  hostname: "postgres",
-  database: "udraw_test#{System.get_env("MIX_TEST_PARTITION")}",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
+  database: "udraw_test#{System.get_env("MIX_TEST_PARTITION")}.db",
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
--- lib/udraw/repo.ex
+++ lib/udraw/repo.ex
@@ -1,5 +1,9 @@
 defmodule Udraw.Repo do
   use Ecto.Repo,
     otp_app: :udraw,
-    adapter: Ecto.Adapters.Postgres
+    adapter:
+      if(Mix.env() in [:dev, :test],
+        do: Ecto.Adapters.SQLite3,
+        else: Ecto.Adapters.Postgres
+      )
 end
