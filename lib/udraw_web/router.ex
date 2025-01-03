17efdea582e9123132e3fa3cf1b8d1fc26915a1b
Tim Sullivan <tsullivan@timatooth.com>
2025-01-03 14:55:50 +1300

    Phoenix gen auth

--- lib/udraw_web/router.ex
+++ lib/udraw_web/router.ex
@@ -1,6 +1,8 @@
 defmodule UdrawWeb.Router do
   use UdrawWeb, :router
 
+  import UdrawWeb.UserAuth
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
@@ -8,11 +10,14 @@ defmodule UdrawWeb.Router do
     plug(:put_root_layout, html: {UdrawWeb.Layouts, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug(:accepts, ["json", "image/png"])
     plug(:cors_plug)
+    plug(:fetch_session)
+    plug :fetch_current_user
   end
 
   pipeline :tile_radius_check do
@@ -52,6 +57,44 @@ defmodule UdrawWeb.Router do
 
       live_dashboard("/dashboard", metrics: UdrawWeb.Telemetry)
       forward("/mailbox", Plug.Swoosh.MailboxPreview)
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", UdrawWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{UdrawWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", UdrawWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{UdrawWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", UdrawWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{UdrawWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
